360abceb600f63e50a5fa0fed6ca2013c70b43b1
Soham-Devne <139870538+Soham-Devne@users.noreply.github.com>
2024-09-09 02:07:45 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_Basics/03_datatypeconversion.js
@@ -0,0 +1,22 @@
+let score = true;
+//console.log(typeof score);
+let value = Number(score);
+//console.log(typeof value);
+//console.log(value);
+// "33" => 33
+//"33abc" => NaN
+// true => 1, false =>0
+
+let islogged ="o";
+let boolval=Boolean(islogged);
+//console.log(typeof boolval);
+//console.log(boolval);
+
+//1 => true, 0 => false
+//"" => false, "o" => true
+
+let num =22;
+let strnum=String(num);
+console.log(typeof strnum);
+console.log(strnum);
+

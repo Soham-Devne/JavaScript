9e9996c761297c598e29fbebfbd074dc22bdc618
Soham-Devne <139870538+Soham-Devne@users.noreply.github.com>
2024-09-11 05:37:38 +0000

    math and num

---
+++ .devcontainer/01_Basics/07_numbers_maths.js
@@ -0,0 +1,35 @@
+const score = 400;
+//console.log(score);
+const balance = new Number(600);
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const anum = 500.93;
+//console.log(anum.toPrecision(4));
+
+const hundreda =  1000000;
+//console.log(hundreda.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//console.log(Math)
+//console.log(Math.abs(-54));
+//console.log(Math.round(4.2));
+//console.log(Math.ceil(4.6));
+//console.log(Math.floor(4.9));
+
+//console.log(Math.min(4,2,3,6,7));
+//console.log(Math.max(4,2,3,6,7));
+
+
+//console.log(Math.random());
+//console.log(Math.random()*10+1);
+//console.log(Math.floor(Math.random()*10+1));
+
+const min =10;
+const max =20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));

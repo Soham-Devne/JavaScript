3ec188c10988f5a5ed9bf3d3dd35c01adbab9213
Soham-Devne <139870538+Soham-Devne@users.noreply.github.com>
2024-09-11 05:51:25 +0000

    date time 1

---
+++ .devcontainer/01_Basics/08_date_time.js
@@ -0,0 +1,15 @@
+let myDate = new Date();
+//console.log(myDate.toString());
+/*
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(typeof myDate);*/
+
+const newdate = new Date(2023,0,23);
+console.log(newdate.toString())
